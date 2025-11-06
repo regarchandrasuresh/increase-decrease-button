b6ed6c65b976c4edd58c9ba3dafa379ec87f4896
Suresh Regar <144477857+regarchandrasuresh@users.noreply.github.com>
2025-11-06 20:41:44 +0530

    Create App component with Counte integration

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counte from "./Counte.jsx";
+   
+function App(){
+  return (
+   <div>
+    <Counte/>
+   </div>
+  )
+ }
+
+
+export default App;
