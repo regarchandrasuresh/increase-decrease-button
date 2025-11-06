c7186c6f7b061da8995f7a3f4a54b4b35e66f341
Suresh Regar <144477857+regarchandrasuresh@users.noreply.github.com>
2025-11-06 20:41:11 +0530

    Create Counte.jsx

---
+++ Counte.jsx
@@ -0,0 +1,14 @@
+    import React,{useState} from "react";
+    export default function Counte(){
+        const[count,setCount] = useState(0);
+    return(
+        <div>
+         <h1>{count}</h1>
+         <button onClick={()=>setCount(count + 1)}>Increase</button> 
+         <button onClick={()=>setCount(count - 1)}>Decrease</button>
+         <button onClick={()=>setCount(0)}>Reset</button>
+        
+
+        </div>
+    )
+}
